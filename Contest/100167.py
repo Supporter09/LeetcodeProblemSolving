00b48dffd4ee189b1dd40151e681894b0ab0fafa
Supporter09 <maivannhatminh2005@gmail.com>
2023-12-24 22:41:18 +0700

    Daily LeetCode

--- Contest/100167.py
+++ Contest/100167.py
@@ -1,16 +1,53 @@
-def count_increasing_subarrays(nums):
-    n = len(nums)
-    count = 0
+class SegmentTree:
+    def __init__(self, size):
+        self.size = size
+        self.tree = [0] * (4 * size)
 
+    def update(self, node, start, end, idx, val):
+        if start == end == idx:
+            self.tree[node] += val
+            return
+        mid = (start + end) // 2
+        if idx <= mid:
+            self.update(2 * node, start, mid, idx, val)
+        else:
+            self.update(2 * node + 1, mid + 1, end, idx, val)
+        self.tree[node] = self.tree[2 * node] + self.tree[2 * node + 1]
+
+    def query(self, node, start, end, left, right):
+        if left > end or right < start:
+            return 0
+        if left <= start and end <= right:
+            return self.tree[node]
+        mid = (start + end) // 2
+        left_sum = self.query(2 * node, start, mid, left, right)
+        right_sum = self.query(2 * node + 1, mid + 1, end, left, right)
+        return left_sum + right_sum
+
+def count_not_adjacent_subarrays(arr):
+    n = len(arr)
+    lis = [1] * n
+    segment_tree = SegmentTree(n)
+
+    # LIS Algorithm
+    for i in range(1, n):
+        for j in range(i):
+            if arr[i] > arr[j] and lis[i] < lis[j] + 1:
+                lis[i] = lis[j] + 1
+
+    # Update segment tree with LIS ending at each index
+    for i in range(n - 1, -1, -1):
+        segment_tree.update(1, 0, n - 1, i, 1)
+
+    # Calculate count of strictly not adjacent subarrays using segment tree
+    count = 0
     for i in range(n):
-        j = i + 1
-        while j < n and nums[j] > nums[j - 1]:
-            j += 1
-            count += 1
+        segment_tree.update(1, 0, n - 1, i, -1)
+        count += segment_tree.query(1, 0, n - 1, i + 1, n - 1)
 
     return count
 
 # Example usage:
-nums = [6,5,7,8]
-result = count_increasing_subarrays(nums)
+arr = [1,2,3,4]
+result = count_not_adjacent_subarrays(arr)
 print(result)
--- test.py
+++ test.py
@@ -1,18 +1,98 @@
-def buyChoco(prices, money):
-    # Second way: O(n) => Iterate and find first and second smallest element
-    first = 105
-    second = 106
-    for i in range(len(prices)):
-        if prices[i] < first: 
-            if prices[i] <= first < second:
-                second = first
-            first = prices[i]
+import argparse
+import os
+import random
+import time
+
+BALL = '⏺'
+COLOR = {
+    'blue': '\033[94m',
+    'yellow': '\033[93m',
+    'cyan': '\033[96m',
+    'green': '\033[92m',
+    'magenta': '\033[95m',
+    'white': '\033[97m',
+    'red': '\033[91m'
+}
+STAR = '★'
+
+
+def random_change_char(string, value):
+    indexes = random.sample(range(0, len(string)), value)
+    string = list(string)
+    for idx in indexes:
+        if string[idx] != ' ' and string[idx] == '_':
+            string[idx] = BALL
+    return ''.join(string)
+
+
+def tree(height=13, screen_width=80):
+    star = (STAR, 3*STAR)
+    if height % 2 != 0:
+        height += 1
+    body = ['/_\\', '/_\_\\']
+    trunk = '[___]'
+    begin = '/'
+    end = '\\'
+    pattern = '_/'
+    j = 5
+    for i in range(7, height + 1, 2):
+        middle = pattern + (i - j) * pattern
+        line = ''.join([begin, middle[:-1], end])
+        body.append(line)
+        middle = middle.replace('/', '\\')
+        line = ''.join([begin, middle[:-1], end])
+        body.append(line)
+        j += 1
+
+    return [line.center(screen_width) for line in (*star, *body, trunk)]
+
+
+def balls(tree):
+    for idx, _ in enumerate(tree[:-3], 2):
+        tree[idx] = random_change_char(tree[idx], len(tree[idx])//8)
+    return tree
+
+
+def colored_stars_balls(tree):
+    for idx, _ in enumerate(tree):
+        string = list(tree[idx])
+        for pos, _ in enumerate(string):
+            if string[pos] == STAR:
+                string[pos] = ''.join([COLOR['yellow'], STAR, '\033[0m'])
+            elif string[pos] == BALL:
+                string[pos] = ''.join([random.choice(list(COLOR.values())), BALL, '\033[0m'])
+        tree[idx] = ''.join(string)
+    return tree
+
+
+def cli():
+    parser = argparse.ArgumentParser(prog="Python Christmas Tree",
+                                     epilog="Ctrl-C interrupts the Christmas :-(")
+    parser.add_argument('-s', '--size', default=13, type=int,
+                        help="Tree height. If even it will be subtracted 1. If less than 7, considered 5. Default: 13")
+    parser.add_argument('-w', '--width', default=80, type=int,
+                        help="Screen width. Used to center the tree. Default: 80")
+    parser.add_argument('-t', '--terminal', action='store_true',
+                        help="Uses the terminal size to center the tree. -s and -w will be ignored")
+    args = parser.parse_args()
+
+    if args.terminal:
+        screen_width, height = os.get_terminal_size()
+        height -= 2
+    else:
+        height = args.size
+        screen_width = args.width
+    while True:
+        try:
+            time.sleep(random.uniform(.1, 1))
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print('\n'.join(colored_stars_balls(balls(tree(height, screen_width)))))
+            print(f"\n{'Merry Christmas!! Bơ 🥑':^{screen_width}}", end='\n\n')
+        except KeyboardInterrupt:
+            os.system('cls' if os.name == 'nt' else 'clear')
             
-        elif first <= prices[i] < second:
-            second = prices[i]
+            break
 
-    res = money - (first + second) if first + second <= money else money
-    print(first, second)
-    return res
 
-buyChoco([69,91,78,19,40,13], 94)
+if __name__ == '__main__':
+    cli()
