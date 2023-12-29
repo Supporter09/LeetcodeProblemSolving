b2cd043eb6045046fc1ce16b79655aafe73917d3
Supporter09 <maivannhatminh2005@gmail.com>
2023-12-29 09:55:38 +0700

    Leetcode hard problem

---
+++ Daily_Leetcode/Hard_Not_Done_Yet/1335.Minimum_Difficulty_of_a_Job_Schedule.py
@@ -0,0 +1,26 @@
+# if number of jobs < d => can not find a schedule for the jobs
+# Find intervals satisfied the sum of maximum el of each intervals in minimum
+
+class Solution:
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        jobCount = len(jobDifficulty)    
+        if jobCount < d:
+            return -1
+
+        @lru_cache(None)
+        def topDown(jobIndex: int, remainDayCount: int) -> int:
+            remainJobCount = jobCount - jobIndex
+            if remainDayCount == 1:
+                return max(jobDifficulty[jobIndex:])
+            
+            if remainJobCount == remainDayCount:
+                return sum(jobDifficulty[jobIndex:])
+
+            minDiff = float('inf')
+            maxToday = 0
+            for i in range(jobIndex, jobCount - remainDayCount + 1):
+                maxToday = max(maxToday, jobDifficulty[i])
+                minDiff = min(minDiff, maxToday + topDown(i+1, remainDayCount-1))
+            return minDiff
+
+        return topDown(0, d)
